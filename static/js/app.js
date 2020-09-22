4cf2abc7e18d47e9752dfcb0b39ebdb88866f7cd
Lauren Stein <lmstein22@gmail.com>
2020-09-21 22:10:20 -0400

    form function v1

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,7 @@ var dataTable = d3.select("tbody");
 
 //console.log(sights);
 
+//Add entries from data to html table
 sights.forEach(function (ufoSighting) {
   var row = dataTable.append("tr");
   Object.entries(ufoSighting).forEach(function ([key, value]) {
@@ -13,3 +14,32 @@ sights.forEach(function (ufoSighting) {
     cell.text(value);
   });
 });
+
+//Access the form for filtering the results of the table
+//Create a variable that references the input form
+var inputForm = d3.select(".form-control");
+
+//Create a variable for the Filter table button
+var filterButton = d3.select("#filter-btn");
+
+//Create event handlers for hitting enter on form and clicking the button to run function
+inputForm.on("submit", runFilter);
+filterButton.on("click", runFilter);
+
+//Create a runFilter function
+function runFilter() {
+  d3.event.preventDefault();
+
+  //Create variable for input element and input value
+  var inputElement = d3.select(".form-control");
+  var inputValue = inputElement.property("value");
+
+  var filterDate = sights.filter((i) => i.datetime === inputValue);
+  filterDate.forEach(function (ufoSighting) {
+    var row = dataTable.append("tr");
+    Object.entries(ufoSighting).forEach(function ([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
